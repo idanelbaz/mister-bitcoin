ab09a9f229623160450c217fc4fe57bff274a68f
idan <idanelbaz@gmail.com>
2019-08-05 20:51:47 +0300

    Updates

---
+++ precache-manifest.81adba9906ec2d8b563a150462442ba5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07e9aa04270f692183cc20a78b778b05",
+    "url": "/mister-bitcoin/index.html"
+  },
+  {
+    "revision": "40839c1d86d8c05f5e1c",
+    "url": "/mister-bitcoin/static/css/main.12f1a32d.chunk.css"
+  },
+  {
+    "revision": "73caa9ffb494b0dbcce7",
+    "url": "/mister-bitcoin/static/js/2.95b9e8bb.chunk.js"
+  },
+  {
+    "revision": "40839c1d86d8c05f5e1c",
+    "url": "/mister-bitcoin/static/js/main.11898e00.chunk.js"
+  },
+  {
+    "revision": "9590c752a00658eae37c",
+    "url": "/mister-bitcoin/static/js/runtime~main.173c7f6e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mister-bitcoin/precache-manifest.a5c48b2c29c1832702dbf856e46b7beb.js"
+  "/mister-bitcoin/precache-manifest.81adba9906ec2d8b563a150462442ba5.js"
 );
 
 self.addEventListener('message', (event) => {
